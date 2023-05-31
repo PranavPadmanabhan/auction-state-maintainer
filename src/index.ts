7fe7336e78f1b0037efcc982f42370df4d8d5dc6
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-05-31 12:23:20 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import "dotenv/config"
+import { ListenAuctions } from './utils/helper-functions';
 
-import mongoose from "mongoose";
-import 'dotenv/config'
-import { ListenAuctions } from "./utils/helper-functions";
+const app = express();
+
+
+
+const port = process.env.PORT
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
 
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`mongoDB connected..`)
-    ListenAuctions()
 })
 
+app.listen(port, () => {
+    console.log(`app running at http://localhost:${port}`)
+    ListenAuctions()
+})  
